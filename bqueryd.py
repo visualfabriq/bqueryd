4dc171b251c86c2b9fe1bcdefecdc62f1b05cdc9
Etienne Posthumus <eposthumus@visualfabriq.com>
2016-12-16 14:05:25 +0100

    Basic combination of multiple filenames

--- bqueryd.py
+++ bqueryd.py
@@ -13,10 +13,13 @@ import traceback
 import json
 import cPickle
 import random
+import pandas as pd
 
 def msg_factory(msg):
     if type(msg) is str:
         msg = json.loads(msg)
+    if not msg:
+        return Message()
     msg_mapping = {'calc': CalcMessage, 'rpc': RPCMessage, 'error': ErrorMessage,
                    'worker_register': WorkerRegisterMessage, None: Message }
     msg_class = msg_mapping.get(msg.get('msg_type'))
@@ -112,9 +115,6 @@ class WorkerNode(object):
             buf = 'zzzzz'
         else:
             filename = kwargs.pop('_filename')
-            groupby_cols = kwargs.get('groupby_cols')
-            agg_list = kwargs.get('agg_list')
-
             rootdir = os.path.join(self.data_dir, filename)
             if not os.path.exists(rootdir):
                 msg['payload'] = 'Path %s does not exist' % rootdir
@@ -143,18 +143,24 @@ class QNode(object):
 
         self.msg_count = 0
         self.rpc_results = [] # buffer of results that are ready to be returned to callers
+        self.rpc_segments = {} # Certain RPC calls get split and divided over workers, this dict tracks the original RPCs
         self.worker_map = {}  # maintain a list of connected workers TODO get rid of unresponsive ones...
+        self.files_map = {} # shows on which workers a file is available on
 
     def handle_sink(self, msg):
         if isinstance(msg, WorkerRegisterMessage):
             tmp = {'last_seen': time.time(), 'wrm':msg}
-            self.worker_map[msg['worker_id']] = tmp
-            logging.debug('QNode Worker registered %s' % msg.get('worker_id'))
+            worker_id = msg.get('worker_id')
+            self.worker_map[worker_id] = tmp
+            logging.debug('QNode Worker registered %s' % worker_id)
+            for filename in msg.get('data_files', []):
+                self.files_map.setdefault(filename, set()).add(worker_id)
             return
 
         # Every msg needs a token, otherwise we don't know who the reply goes to
         if 'token' not in msg:
-            raise Exception('Every msg needs a token')
+            logging.error('Every msg needs a token')
+            return
         logging.debug('QNode Sink received %s' % msg.get('token', '?'))
         self.rpc_results.append(msg)
 
@@ -189,12 +195,7 @@ class QNode(object):
                     msg = msg_factory(msg)
                     self.handle_rpc(msg)
                 if socks.get(self.rpc) & zmq.POLLOUT:
-                    while self.rpc_results:
-                        msg = self.rpc_results.pop()
-                        msg_id = binascii.unhexlify(msg.get('token'))
-                        tmp = [msg_id, '', json.dumps(msg)]
-                        self.rpc.send_multipart(tmp)
-                        logging.debug('QNode Msg handled %s' % msg.get('token', '?'))
+                    self.process_sink_results()
 
             except KeyboardInterrupt:
                 logging.debug('Stopped from keyboard')
@@ -209,6 +210,36 @@ class QNode(object):
         for x in self.worker_map:
             self.ventilator.send_multipart([x, Message({'payload': 'kill'}).to_json()] )
         self.running = False
+
+    def process_sink_results(self):
+        while self.rpc_results:
+            msg = self.rpc_results.pop()
+            # If this was a message to be combined, there should be a parent_token
+            if 'parent_token' in msg:
+                parent_token = msg['parent_token']
+                original_rpc = self.rpc_segments.get(parent_token)
+
+                params = msg.get_from_binary('params') or {}
+                filename = params['kwargs']['_filename']
+                original_rpc['filenames'][filename] = msg.get_from_binary('result')
+
+                if len([True for v in original_rpc['filenames'].values() if v is None]) < 1: # Check to see that there are no filenames with no Result yet
+                    new_result = pd.concat(original_rpc['filenames'].values(), ignore_index=True)
+                    groupby_cols, agg_list = params['args']
+                    new_result = new_result.groupby(groupby_cols)[agg_list] # TODO this needs to be generic and maybe done in a worker due to memory issues?
+
+                    # We have received all the segment, send a reply to RPC caller
+                    msg = original_rpc['msg']
+                    msg.add_as_binary('result', new_result)
+                    del self.rpc_segments[parent_token]
+                else:
+                    # This was a segment result move on
+                    continue
+
+            msg_id = binascii.unhexlify(msg.get('token'))
+            tmp = [msg_id, '', json.dumps(msg)]
+            self.rpc.send_multipart(tmp)
+            logging.debug('QNode Msg handled %s' % msg.get('token', '?'))
 
     def handle_rpc(self, msg):
         # Every msg needs a token, otherwise we don't know wo the reply goes to
@@ -227,17 +258,44 @@ class QNode(object):
             self.rpc_results.append(msg)
             self.kill()
         elif msg['payload'] in ('groupby', 'where_terms', 'sleep'):
-            # Pick a random worker_id to send a message to for now, TODO add some kind of load-balancing & affinity
-            worker_id = random.choice(self.worker_map.keys())
-            # Send a calc message to the workers on the ventilator for the number of shards for this filename
-            # TODO Add a tracking of which requests have been sent out to the worker, and do retries with timeouts
-            self.ventilator.send_multipart([worker_id, json.dumps(msg)])
+            data = self.handle_calc_message(msg)
         else:
             data = "Sorry, I don't understand you"
 
         if data:
             msg.add_as_binary('result', data)
             self.rpc_results.append(msg)
+
+    def handle_calc_message(self, msg):
+        # Store the original message in the buf
+
+
+        params = msg.get_from_binary('params') or {}
+        filenames = params.get('kwargs', {}).get('_filenames', [])
+        if not filenames:
+            return 'Error, kwargs in msg has no _filenames parameter?'
+
+        parent_token = msg['token']
+        self.rpc_segments[parent_token] = {'msg': msg_factory(msg.copy()),
+                                           'filenames': dict([(x,None) for x in filenames]) }
+
+        for filename in filenames:
+            if filename and filename not in self.files_map:
+                return 'Sorry, filename %s was not found' % filename
+
+            params['kwargs']['_filename'] = filename
+            msg.add_as_binary('params', params)
+
+            # Make up a new token for the message sent to the workers, and collect the responses using that id
+            msg['parent_token'] = parent_token
+            msg['token'] = binascii.hexlify(os.urandom(8))
+
+            # Pick a random worker_id to send a message to for now, TODO add some kind of load-balancing & affinity
+            worker_id = random.choice(self.worker_map.keys())
+            # Send a calc message to the workers on the ventilator for the number of shards for this filename
+            # TODO Add a tracking of which requests have been sent out to the worker, and do retries with timeouts
+            self.ventilator.send_multipart([worker_id, json.dumps(msg)])
+
 
 class RPC(object):
 
