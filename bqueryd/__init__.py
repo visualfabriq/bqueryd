816593931484d372ff4b9103da2e4a0043852a6d
Etienne Posthumus <eposthumus@visualfabriq.com>
2017-01-05 16:36:57 +0100

    Remove threading methody

--- bqueryd/__init__.py
+++ bqueryd/__init__.py
@@ -1,21 +1,8 @@
 import logging
 logging.basicConfig(format="%(asctime)s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
 
-__version__ = 0.2
+__version__ = 0.3
 
 from rpc import RPC, RPCError
 from controller import ControllerNode
 from worker import WorkerNode
-
-def threaded():
-    import threading
-
-    w = WorkerNode()
-    worker = threading.Thread(target=w.go)
-    q = ControllerNode()
-    queue = threading.Thread(target=q.go)
-
-    worker.start()
-    queue.start()
-
-    return q, w
