89f7dfe141dce2c9b5474d0afddf32c5048b9b06
Etienne Posthumus <eposthumus@visualfabriq.com>
2017-01-27 23:10:42 +0100

    Quiten boto

--- bqueryd/__init__.py
+++ bqueryd/__init__.py
@@ -2,8 +2,10 @@ import os
 import logging
 logging.basicConfig(format="%(asctime)s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
 logger = logging.getLogger('bqueryd')
+b = logging.getLogger('boto')
+b.setLevel(logging.INFO)
 
-__version__ = 0.4
+__version__ = 0.5
 DEFAULT_DATA_DIR = '/srv/bcolz/'
 INCOMING = os.path.join(DEFAULT_DATA_DIR, 'incoming')
 if not os.path.exists(INCOMING):
