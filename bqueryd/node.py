bad9909ac1149063cf97fc03787c203a17308552
Etienne Posthumus <eposthumus@visualfabriq.com>
2017-01-31 13:56:28 +0100

    Use var named "rpc" and not "a"

--- bqueryd/node.py
+++ bqueryd/node.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         bqueryd.WorkerNode(redis_url=redis_url, loglevel=loglevel).go()
     else:
         if len(sys.argv) > 1 and sys.argv[1].startswith('tcp:'):
-            a = bqueryd.RPC(address=sys.argv[1], redis_url=redis_url, loglevel=loglevel)
+            rpc = bqueryd.RPC(address=sys.argv[1], redis_url=redis_url, loglevel=loglevel)
         else:
-            a = bqueryd.RPC(redis_url=redis_url, loglevel=loglevel)
-        sys.stderr.write('Run this script with python -i , and then you will have a variable named "a" as a connection.\n')
+            rpc = bqueryd.RPC(redis_url=redis_url, loglevel=loglevel)
+        sys.stderr.write('Run this script with python -i , and then you will have a variable named "rpc" as a connection.\n')
