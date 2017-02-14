e4473307a98ec536dde508dc1824761c1268abbc
Etienne Posthumus <eposthumus@visualfabriq.com>
2017-02-14 16:32:45 +0100

    Set MAX_MESSAGES to 200 and show own pid in info call

--- bqueryd/worker.py
+++ bqueryd/worker.py
@@ -26,7 +26,7 @@ DATA_FILE_EXTENSION = '.bcolz'
 DATA_SHARD_FILE_EXTENSION = '.bcolzs'
 POLLING_TIMEOUT = 5000  # timeout in ms : how long to wait for network poll, this also affects frequency of seeing new controllers and datafiles
 WRM_DELAY = 20  # how often in seconds to send a WorkerRegisterMessage
-MAX_MESSAGES = 20  # after how many messages should the controller be restarted
+MAX_MESSAGES = 200  # after how many messages should the controller be restarted
 MAX_MESSAGES = int(MAX_MESSAGES + 0.30 * MAX_MESSAGES * (random.random() - 0.5))  # randomize actual amount
 bcolz.set_nthreads(1)
 
@@ -104,6 +104,7 @@ class WorkerNode(object):
         wrm['controllers'] = self.controllers.values()
         wrm['uptime'] = int(time.time() - self.start_time)
         wrm['msg_count'] = self.msg_count
+        wrm['pid'] = os.getpid()
         return wrm
 
     def heartbeat(self):
