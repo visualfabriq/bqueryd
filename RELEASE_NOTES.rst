154d9547b2109ce5ba12ad32384a9a50148246b9
carstvaartjes@gmail.com <JunoAdmin76>
2018-01-19 17:21:36 +0100

    pypi

---
+++ RELEASE_NOTES.rst
@@ -0,0 +1,13 @@
+Release  0.2.1
+=======================
+- Manifest fix
+
+Release  0.2.0
+=======================
+- Initial release
+
+.. Local Variables:
+.. mode: rst
+.. coding: utf-8
+.. fill-column: 72
+.. End:
--- setup.py
+++ setup.py
@@ -1,25 +1,130 @@
-from distutils.core import setup
+from __future__ import absolute_import
 
-setup(name='bqueryd',
-      version='0.10.1',
-      description='A companion library to make bquery distributed',
-      maintainer='Carst Vaartjes',
-      maintainer_email='cvaartjes@visualfabriq.com',
-      url='https://github.com/visualfabriq/bqueryd',
-      license='MIT',
-      packages=['bqueryd'],
-      install_requires = [
-          'bquery>=0.2.10',
-          'pyzmq>=16.0.2',
-          'redis>=2.10.5',
-          'boto>=2.43.0',
-          'smart_open>=1.3.5',
-          'netifaces>=0.10.5',
-          'configobj>=5.0.6'
-      ],
-      entry_points={
-            'console_scripts': [
-                  'bqueryd = bqueryd.node:main'
-            ]
-            },
-      )
+########################################################################
+#       File based on https://github.com/Blosc/bcolz
+########################################################################
+#
+# License: BSD
+# Created: October 5, 2015
+#       Author:  Carst Vaartjes - cvaartjes@visualfabriq.com
+#
+########################################################################
+import codecs
+import os
+
+from setuptools import setup, Extension, find_packages
+from os.path import abspath
+from sys import version_info as v
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+# Check this Python version is supported
+if any([v < (2, 6), (3,) < v < (3, 3)]):
+    raise Exception("Unsupported Python version %d.%d. Requires Python >= 2.7 "
+                    "or >= 3.3." % v[:2])
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+def get_version():
+    with codecs.open(abspath('VERSION'), "r", "utf-8") as f:
+        return f.readline().rstrip('\n')
+
+# Sources & libraries
+inc_dirs = [abspath('bqueryd')]
+try:
+    import numpy as np
+    inc_dirs.append(np.get_include())
+except ImportError as e:
+    pass
+lib_dirs = []
+libs = []
+def_macros = []
+sources = []
+
+cmdclass = {'build_ext': build_ext}
+
+optional_libs = ['numexpr>=1.4.1']
+install_requires = [
+    'bquery>=0.2.10',
+    'pyzmq>=16.0.2',
+    'redis>=2.10.5',
+    'boto>=2.43.0',
+    'smart_open>=1.3.5',
+    'netifaces>=0.10.5',
+    'configobj>=5.0.6'
+]
+setup_requires = []
+tests_requires = []
+extras_requires = []
+ext_modules = []
+package_data = {}
+classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+
+setup(
+    name="bqueryd",
+    version=get_version(),
+    description='A distribution framework for Bquery',
+    long_description=read("README.md"),
+    classifiers=classifiers,
+    author='Carst Vaartjes',
+    author_email='cvaartjes@visualfabriq.com',
+    maintainer='Carst Vaartjes',
+    maintainer_email='cvaartjes@visualfabriq.com',
+    url='https://github.com/visualfabriq/bqueryd',
+    license='GPL2',
+    platforms=['any'],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_requires,
+    extras_require=dict(
+        optional=extras_requires,
+        test=tests_requires
+    ),
+    packages=find_packages(),
+    package_data=package_data,
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'bqueryd = bqueryd.node:main'
+        ]
+    },
+)
+
